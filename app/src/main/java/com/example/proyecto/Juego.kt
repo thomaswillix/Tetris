b3511cb7699bfcf3b83c63fd8b39cd219d33164f
Thomas <thomaswfds@gmail.com>
2023-12-26 19:39:12 +0100

    26/12/2023

---
+++ app/src/main/java/com/example/proyecto/Juego.kt
@@ -0,0 +1,13 @@
+package com.example.proyecto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Juego : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.game)
+
+
+    }
+}
--- app/src/main/java/com/example/proyecto/MainActivity.kt
+++ app/src/main/java/com/example/proyecto/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.proyecto
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val play = findViewById<Button>(R.id.btPlay)
+        play.setOnClickListener{
+            startActivity(Intent(this, Juego::class.java))
+        }
     }
 }
