89cad7007eb7112b48d4edf47d81f5b788e1dcba
Thomas <thomaswfds@gmail.com>
2023-12-29 19:25:40 +0100

    29/12/2023

--- app/src/main/java/com/example/proyecto/MainActivity.kt
+++ app/src/main/java/com/example/proyecto/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.proyecto
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -9,9 +10,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val git  = findViewById<Button>(R.id.github)
         val play = findViewById<Button>(R.id.btPlay)
         play.setOnClickListener{
             startActivity(Intent(this, Game::class.java))
         }
+        git.setOnClickListener {
+            goLink("https://github.com/thomaswillix/Tetris");
+        }
+    }
+
+    private fun goLink(string: String) {
+        val uri = Uri.parse(string)
+        startActivity(Intent(Intent.ACTION_VIEW,uri))
     }
 }
