b062fe689917bbf13bac916c8bd635bfc1140588
Thomas <117021963+thomaswillix@users.noreply.github.com>
2024-01-19 13:26:02 +0100

    18/01/2024

---
+++ app/src/main/java/com/example/proyecto/AdminSQL.kt
@@ -0,0 +1,18 @@
+package com.example.proyectomviles
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteDatabase.CursorFactory
+import android.database.sqlite.SQLiteOpenHelper
+
+class AdminSQL(context: Context, name: String, factory: CursorFactory?, version: Int) :
+    SQLiteOpenHelper(context, name, factory, version) {
+    val TABLE_NAME= " HighScores "
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL("create table "+TABLE_NAME+" (user text primary key, points integer)")
+    }
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        db.execSQL("drop table if exists "+ TABLE_NAME)
+        onCreate(db)
+    }
+}
